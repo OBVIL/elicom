435d8eff0882a298428116828d0d3c638b4924ab
LaureRossignol96 <58423339+LaureRossignol96@users.noreply.github.com>
2020-07-07 18:16:28 +0200

    Update extraction-elicom_lamennais.py

--- extraction_cor_stage2020/cor_lamennais/script/extraction-elicom_lamennais.py
+++ extraction_cor_stage2020/cor_lamennais/script/extraction-elicom_lamennais.py
@@ -15,7 +15,7 @@ from bs4 import BeautifulSoup, NavigableString, Tag
 from lxml import etree
 from lxml.builder import ElementMaker
 
-RE_CHIF_AR = re.compile(r"^[0-9]{1,3}\.") #pour matcher les débuts de lettres. 153 matches au lieu de 197
+RE_CHIF_AR = re.compile(r"(^[0-9]{1,3})(\.)") #pour matcher les débuts de lettres. 153 matches au lieu de 197
 #RE_DEST1 = re.compile(r"(A [A-Z][A-Z.]+)|^(?:A[A-Z.]+)")# 215 matches non complets
 RE_NOTES = re.compile(r"^\d [A-Z].+")
 #RE_PLACENAME = re.compile(r"[A Vitrolles,|Rennes|Château de Verneuil,|Château de Fleury,|Saint-Sauveur,|Versailles|A la Chenaie|La Chenaie,|Lyon|Rome|Genève|Paris|Saint-Brieuc|Dinan,]")
@@ -48,21 +48,12 @@ for p in soup.body.find_all('p'):
 
 E = ElementMaker(namespace="http://www.tei-c.org/ns/1.0", nsmap={None: "http://www.tei-c.org/ns/1.0"})
 for i, lettre in enumerate(lettres[1:]):
-    to, date, signature = "", "", ""
-    corps = []
-    try:
-        to, date, *corps, signature = lettre
-    except ValueError:
-        continue
-
-E = ElementMaker(namespace="http://www.tei-c.org/ns/1.0", nsmap={None: "http://www.tei-c.org/ns/1.0"})
-for i, lettre in enumerate(lettres[1:]):
-	n, to, date, signature = "", "", "", ""
+	to, date, signature = "", "", ""
+	corps = []
 	try:
-		n, *corps, signature = lettre
-		if RE_DEST.findall(n): 
+		to, date, *corps, signature = lettre
+		if RE_DEST.findall(to):
 			pass
-
 
 	except ValueError:
 		pass
